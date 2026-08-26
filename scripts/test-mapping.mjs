264cd06f6d1a79d35756a2609cd0cfadca6b2d35
chriscomputerwissen <chris.computerwissen@gmail.com>
2026-08-26 20:44:31 +0200

    Update test-mapping.mjs

--- scripts/test-mapping.mjs
+++ scripts/test-mapping.mjs
@@ -21,16 +21,25 @@ console.log(`${objects.length} Testzeilen, ${products.length} gemappte Produkte:
 console.log(JSON.stringify(products, null, 2));
 
 const assertions = [
-  [products.length === 4, "Es sollten 4 Produkte gemappt werden"],
+  [products.length === 7, "Es sollten 7 Produkte gemappt werden"],
   [products[0].deviceType === "laptop", "Zeile 1 sollte ein Laptop sein"],
   [products[0].cpuClass === "mittel", "i5 sollte als 'mittel' erkannt werden"],
   [products[0].ramGB === 16, "16GB RAM sollte erkannt werden"],
+  [products[0].os === "windows", "Windows 11 im Text sollte os='windows' ergeben"],
+  [products[0].shop === "notebooksbilliger.de", "Zeile 1 sollte shop='notebooksbilliger.de' bekommen"],
   [products[1].hasGPU === true, "RTX 4060 sollte hasGPU=true ergeben"],
   [products[1].useCases.includes("gaming"), "Gaming-Notebook sollte useCase 'gaming' bekommen"],
   [products[2].deviceType === "desktop", "Buero Desktop-PC sollte als 'desktop' erkannt werden"],
   [products[2].cpuClass === "einsteiger", "i3 sollte als 'einsteiger' erkannt werden"],
   [products[3].cpuClass === "mittel", "Apple M3 (Basis) sollte als 'mittel' erkannt werden"],
   [products[3].mobility === 5, "13 Zoll MacBook sollte hohe Mobilitaet (5) bekommen"],
+  [products[3].os === "macos", "Apple MacBook sollte os='macos' bekommen"],
+  [products[4].deviceType === "desktop", "Intel NUC Mini PC sollte als 'desktop' erkannt werden"],
+  [products[5].deviceType === "desktop", "Barebone Gaming-PC sollte als 'desktop' erkannt werden"],
+  [products[5].os === "ohne", "'ohne Betriebssystem' im Text sollte os='ohne' ergeben"],
+  [products[6].shop === "Cyberport", "Zeile 7 (Cyberport-Feed) sollte shop='Cyberport' bekommen"],
+  [products[6].brand === "Cyberport", "Ohne erkannte Hersteller-Marke sollte brand auf den Händlernamen zurückfallen"],
+  [products[6].id === "11657-2001", "Bei kombiniertem Multi-Händler-Feed sollte die ID mit merchant_id geprefixt werden"],
   [products.every((p) => p.affiliateUrl.startsWith("https://www.awin1.com")), "affiliateUrl sollte der Awin-Tracking-Link sein"]
 ];
 
